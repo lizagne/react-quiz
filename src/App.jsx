e91c3eaf1153cb454904bf0accef562871369f4f
Liz Verde <>
2017-12-01 10:10:16 +0000

    Challenge 2: creating a component which displays Even if it's clicked an even amount of times and odd if it's clicked an odd amount of times: can't quite get it to work fully, but will come back to it

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import React, { Component } from 'react';
 
 
 import Multiplier from "./Multiplier"
+import EvenClicks from "./EvenClicks"
 
 class App extends Component {
   render() {
     return (
-
-      <Multiplier x={ 10 } y={ 7 } />
+    	
+    	<div>
+	    	<Multiplier x={ 10 } y={ 7 } />
+	    	<EvenClicks />
+    	</div>
      
     );
   }
