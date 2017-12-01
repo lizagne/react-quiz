ba54f3a5ba75989faddd8500a640fed033d6146f
Liz Verde <>
2017-12-01 11:01:55 +0000

    Adding up the passwords one before it's actually working: I'll come back to it this afternoon

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import Multiplier from "./Multiplier"
 import EvenClicks from "./EvenClicks"
 import CountBy from "./CountBy"
 import HideMe from "./HideMe"
+import MinimumLength from "./MinimumLength"
 
 class App extends Component {
   render() {
     return (
 
     	<div>
-	    	<Multiplier x={ 10 } y={ 7 } />
+	    	<Multiplier x={ 5 } y={ 7 } />
 	    	<hr />
 	    	<EvenClicks />
 	    	<hr />
 	    	<CountBy step={ 5 } />
 	    	<hr />
 	    	<HideMe>Blah blah blah</HideMe> 
+	    	<hr />
+	    	<MinimumLength minimumLength={ 30 } />
     	</div>
      
     );
@@ -27,3 +30,6 @@ class App extends Component {
 }
 
 export default App;
+
+
+
