e033df8c8f0a396ad4b0112f242820e4d29eddff
Liz Verde <>
2017-12-01 10:19:43 +0000

    Challenge 3: the react quiz CountBy challenge, adding five each time a button is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import React, { Component } from 'react';
 
 import Multiplier from "./Multiplier"
 import EvenClicks from "./EvenClicks"
+import CountBy from "./CountBy"
 
 class App extends Component {
   render() {
     return (
-    	
+
     	<div>
 	    	<Multiplier x={ 10 } y={ 7 } />
+	    	<hr />
 	    	<EvenClicks />
+	    	<hr />
+	    	<CountBy step={ 5 } />
+	    	<hr />
     	</div>
      
     );
